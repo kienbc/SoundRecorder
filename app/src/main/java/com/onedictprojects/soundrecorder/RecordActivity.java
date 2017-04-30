2894803a7812850ed6760fbfddc12c1a0284d048
Kien C. Bui <kiencbui@gmail.com>
2017-04-30 20:12:55 +0700

    tắc visualizer

--- app/src/main/java/com/onedictprojects/soundrecorder/RecordActivity.java
+++ app/src/main/java/com/onedictprojects/soundrecorder/RecordActivity.java
@@ -67,12 +67,12 @@ public class RecordActivity extends AppCompatActivity {
 
     // visualizer
 
-    private CDrawer.CDrawThread mDrawThread;
-    private CDrawer mdrawer;
-    private View.OnClickListener listener;
-    private Boolean m_bStart = Boolean.valueOf(false);
-    private Boolean recording;
-    private CSampler sampler;
+//    private CDrawer.CDrawThread mDrawThread;
+//    private CDrawer mdrawer;
+//    private View.OnClickListener listener;
+//    private Boolean m_bStart = Boolean.valueOf(false);
+//    private Boolean recording;
+//    private CSampler sampler;
 
 
     // visualizer
@@ -142,128 +142,128 @@ public class RecordActivity extends AppCompatActivity {
 
         //========= Visualizer ============//
 
-        mdrawer = (CDrawer) findViewById(R.id.drawer);
-        m_bStart = Boolean.valueOf(false);
-
-        while (true) {
-            recording = Boolean.valueOf(false);
-            run();
-            return;
-        }
+//        mdrawer = (CDrawer) findViewById(R.id.drawer);
+//        m_bStart = Boolean.valueOf(false);
+//
+//        while (true) {
+//            recording = Boolean.valueOf(false);
+//            run();
+//            return;
+//        }
 
         //========= Visualizer ============//
 
     }
 
-    @Override
-    protected void onPause() {
-        sampler.SetRun(Boolean.valueOf(false));
-        mDrawThread.setRun(Boolean.valueOf(false));
-        sampler.SetSleeping(Boolean.valueOf(true));
-        mDrawThread.SetSleeping(Boolean.valueOf(true));
-        Boolean.valueOf(false);
-        super.onPause();
+//    @Override
+//    protected void onPause() {
+//        sampler.SetRun(Boolean.valueOf(false));
+//        mDrawThread.setRun(Boolean.valueOf(false));
+//        sampler.SetSleeping(Boolean.valueOf(true));
+//        mDrawThread.SetSleeping(Boolean.valueOf(true));
+//        Boolean.valueOf(false);
+//        super.onPause();
+//
+//    }
 
-    }
+//    @Override
+//    protected void onRestart() {
+//        m_bStart = Boolean.valueOf(true);
+//        super.onRestart();
+//    }
 
-    @Override
-    protected void onRestart() {
-        m_bStart = Boolean.valueOf(true);
-        super.onRestart();
-    }
+//    @Override
+//    protected void onResume()
+//    {
+//        System.out.println("onresume");
+//        int i = 0;
+//        while (true)
+//        {
+//            if ((sampler.GetDead2().booleanValue()) && (mdrawer.GetDead2().booleanValue()))
+//            {
+//                System.out.println(sampler.GetDead2() + ", " + mdrawer.GetDead2());
+//                sampler.Restart();
+//                if (!m_bStart.booleanValue())
+//                    mdrawer.Restart(Boolean.valueOf(true));
+//                sampler.SetSleeping(Boolean.valueOf(false));
+//                mDrawThread.SetSleeping(Boolean.valueOf(false));
+//                m_bStart = Boolean.valueOf(false);
+//                super.onResume();
+//                return;
+//            }
+//            try
+//            {
+//                Thread.sleep(500L);
+//                System.out.println("Hang on..");
+//                i++;
+//                if (!sampler.GetDead2().booleanValue())
+//                    System.out.println("sampler not DEAD!!!");
+//                if (!mdrawer.GetDead2().booleanValue())
+//                {
+//                    System.out.println("mDrawer not DeAD!!");
+//                    mdrawer.SetRun(Boolean.valueOf(false));
+//                }
+//                if (i <= 4)
+//                    continue;
+//                mDrawThread.SetDead2(Boolean.valueOf(true));
+//            }
+//            catch (InterruptedException localInterruptedException)
+//            {
+//                localInterruptedException.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    protected void onStart()
+//    {
+//        System.out.println("onstart");
+//        super.onStart();
+//    }
+//
+//    @Override
+//    protected void onStop()
+//    {
+//        System.out.println("onstop");
+//        super.onStop();
+//    }
 
-    @Override
-    protected void onResume()
-    {
-        System.out.println("onresume");
-        int i = 0;
-        while (true)
-        {
-            if ((sampler.GetDead2().booleanValue()) && (mdrawer.GetDead2().booleanValue()))
-            {
-                System.out.println(sampler.GetDead2() + ", " + mdrawer.GetDead2());
-                sampler.Restart();
-                if (!m_bStart.booleanValue())
-                    mdrawer.Restart(Boolean.valueOf(true));
-                sampler.SetSleeping(Boolean.valueOf(false));
-                mDrawThread.SetSleeping(Boolean.valueOf(false));
-                m_bStart = Boolean.valueOf(false);
-                super.onResume();
-                return;
-            }
-            try
-            {
-                Thread.sleep(500L);
-                System.out.println("Hang on..");
-                i++;
-                if (!sampler.GetDead2().booleanValue())
-                    System.out.println("sampler not DEAD!!!");
-                if (!mdrawer.GetDead2().booleanValue())
-                {
-                    System.out.println("mDrawer not DeAD!!");
-                    mdrawer.SetRun(Boolean.valueOf(false));
-                }
-                if (i <= 4)
-                    continue;
-                mDrawThread.SetDead2(Boolean.valueOf(true));
-            }
-            catch (InterruptedException localInterruptedException)
-            {
-                localInterruptedException.printStackTrace();
-            }
-        }
-    }
+//    public void setBuffer(short[] paramArrayOfShort)
+//    {
+//        mDrawThread = mdrawer.getThread();
+//        mDrawThread.setBuffer(paramArrayOfShort);
+//    }
 
-    @Override
-    protected void onStart()
-    {
-        System.out.println("onstart");
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop()
-    {
-        System.out.println("onstop");
-        super.onStop();
-    }
-
-    public void setBuffer(short[] paramArrayOfShort)
-    {
-        mDrawThread = mdrawer.getThread();
-        mDrawThread.setBuffer(paramArrayOfShort);
-    }
-
-    public void run()
-    {
-        try
-        {
-            if (mDrawThread == null)
-            {
-                mDrawThread = mdrawer.getThread();
-            }
-            if (sampler == null)
-                sampler = new CSampler(this);
-            Context localContext = getApplicationContext();
-            Display localDisplay = getWindowManager().getDefaultDisplay();
-            Toast localToast = Toast.makeText(localContext, "Please make some noise..", Toast.LENGTH_LONG);
-            localToast.setGravity(48, 0, localDisplay.getHeight() / 8);
-            localToast.show();
-            mdrawer.setOnClickListener(listener);
-            if (sampler != null){
-                try {
-                    sampler.Init();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-
-                sampler.StartRecording();
-                sampler.StartSampling();
-            }
-        } catch (NullPointerException e) {
-            Log.e("Main_Run", "NullPointer: " + e.getMessage());
-        }
-    }
+//    public void run()
+//    {
+//        try
+//        {
+//            if (mDrawThread == null)
+//            {
+//                mDrawThread = mdrawer.getThread();
+//            }
+//            if (sampler == null)
+//                sampler = new CSampler(this);
+//            Context localContext = getApplicationContext();
+//            Display localDisplay = getWindowManager().getDefaultDisplay();
+//            Toast localToast = Toast.makeText(localContext, "Please make some noise..", Toast.LENGTH_LONG);
+//            localToast.setGravity(48, 0, localDisplay.getHeight() / 8);
+//            localToast.show();
+//            mdrawer.setOnClickListener(listener);
+//            if (sampler != null){
+//                try {
+//                    sampler.Init();
+//                } catch (Exception ex) {
+//                    ex.printStackTrace();
+//                }
+//
+//                sampler.StartRecording();
+//                sampler.StartSampling();
+//            }
+//        } catch (NullPointerException e) {
+//            Log.e("Main_Run", "NullPointer: " + e.getMessage());
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
